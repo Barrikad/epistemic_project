bbd660c21a9081c24bc3af6fc16628cd1782668e
Barrikad <s184459@student.dtu.dk>
2022-11-21 20:17:38 +0100

    semantics and syntax of group knowledge

--- Epistemic_Logic.thy
+++ Epistemic_Logic.thy
@@ -24,9 +24,9 @@ datatype 'i fm
   | Con \<open>'i fm\<close> \<open>'i fm\<close> (infixr \<open>\<^bold>\<and>\<close> 65)
   | Imp \<open>'i fm\<close> \<open>'i fm\<close> (infixr \<open>\<^bold>\<longrightarrow>\<close> 55)
   | K 'i \<open>'i fm\<close>
-  | Ev \<open>'i set\<close> \<open>'i fm\<close>
-  | Co \<open>'i set\<close> \<open>'i fm\<close>
-  | Di \<open>'i set\<close> \<open>'i fm\<close>
+  | Ev \<open>'i list\<close> \<open>'i fm\<close>
+  | Co \<open>'i list\<close> \<open>'i fm\<close>
+  | Di \<open>'i list\<close> \<open>'i fm\<close>
 
 abbreviation TT (\<open>\<^bold>\<top>\<close>) where
   \<open>TT \<equiv> \<^bold>\<bottom> \<^bold>\<longrightarrow> \<^bold>\<bottom>\<close>
@@ -53,9 +53,9 @@ primrec f_size where
   \<open>f_size (p \<^bold>\<and> q) = f_size p + f_size q + 1\<close> |
   \<open>f_size (p \<^bold>\<longrightarrow> q) = f_size p + f_size q + 1\<close> |
   \<open>f_size (K i p) = f_size p + 1\<close> |
-  \<open>f_size (Ev is p) = f_size p + 2\<close> |
-  \<open>f_size (Co is p) = f_size p + 1\<close> |
-  \<open>f_size (Di is p) = f_size p + 2\<close> 
+  \<open>f_size (Ev g p) = f_size p + 2\<close> |
+  \<open>f_size (Co g p) = f_size p + 1\<close> |
+  \<open>f_size (Di g p) = f_size p + 2\<close> 
 
 primrec common_count where 
   \<open>common_count \<^bold>\<bottom> = 0\<close> |
@@ -64,24 +64,16 @@ primrec common_count where
   \<open>common_count (p \<^bold>\<and> q) = common_count p + common_count q\<close> |
   \<open>common_count (p \<^bold>\<longrightarrow> q) = common_count p + common_count q\<close> |
   \<open>common_count (K i p) = common_count p\<close> |
-  common_count_ev: \<open>common_count (Ev is p) = common_count p\<close> |
-  \<open>common_count (Co is p) = common_count p + 1\<close> |
-  \<open>common_count (Di is p) = common_count p\<close> 
+  \<open>common_count (Ev g p) = common_count p\<close> |
+  \<open>common_count (Co g p) = common_count p + 1\<close> |
+  \<open>common_count (Di g p) = common_count p\<close> 
 
 primrec Ev_n where
   \<open>Ev_n g p 0 = p\<close> |
-  \<open>Ev_n g p (Suc n) = Ev_n g (Ev g p) n\<close>
+  \<open>Ev_n g p (Suc n) = Ev g (Ev_n g p n)\<close>
 
 lemma common_count_EV_n: \<open>common_count (Ev_n g p x) < Suc (common_count p)\<close>
-proof (induct x)
-  case 0
-  then show ?case by simp
-next
-  case (Suc x)
-  have \<open>common_count (Ev_n g p (Suc x)) = common_count (Ev_n g (Ev g p) x)\<close>
-    
-  then show ?case sorry
-qed
+by (induct x) auto
 
 function semantics :: \<open>('i, 'w) kripke \<Rightarrow> 'w \<Rightarrow> 'i fm \<Rightarrow> bool\<close> (\<open>_, _ \<Turnstile> _\<close> [50, 50, 50] 50) where
   \<open>M, w \<Turnstile> \<^bold>\<bottom> \<longleftrightarrow> False\<close>
@@ -90,16 +82,15 @@ function semantics :: \<open>('i, 'w) kripke \<Rightarrow> 'w \<Rightarrow> 'i f
 | \<open>M, w \<Turnstile> p \<^bold>\<and> q \<longleftrightarrow> M, w \<Turnstile> p \<and> M, w \<Turnstile> q\<close>
 | \<open>M, w \<Turnstile> p \<^bold>\<longrightarrow> q \<longleftrightarrow> M, w \<Turnstile> p \<longrightarrow> M, w \<Turnstile> q\<close>
 | \<open>M, w \<Turnstile> K i p \<longleftrightarrow> (\<forall>v \<in> \<W> M \<inter> \<K> M i w. M, v \<Turnstile> p)\<close>
-| \<open>M, w \<Turnstile> Ev g p \<longleftrightarrow> (\<forall> i \<in> g. M, w \<Turnstile> K i p)\<close>
+| \<open>M, w \<Turnstile> Ev g p \<longleftrightarrow> (\<forall> i \<in> set g. M, w \<Turnstile> K i p)\<close>
 | \<open>M, w \<Turnstile> Co g p \<longleftrightarrow> (\<forall> n. M, w \<Turnstile> Ev_n g p n)\<close>
-| \<open>M, w \<Turnstile> Di g p \<longleftrightarrow> (\<forall> i \<in> g. M, w \<Turnstile> K i p)\<close>
+| \<open>M, w \<Turnstile> Di g p \<longleftrightarrow> (\<forall>v \<in> \<W> M \<inter> (\<Inter> i \<in> set g. \<K> M i w). M, v \<Turnstile> p)\<close>
   by pat_completeness auto
 termination 
 proof (relation \<open>measures [\<lambda> (_,_,p). common_count p, \<lambda> (_,_,p).f_size p]\<close>) 
   show \<open>\<And>M w is p x. ((M, w, Ev_n is p x), M, w, Co is p) \<in> measures [\<lambda>(_, _, p). common_count p, \<lambda>(_, _, p). f_size p]\<close>
     using common_count_EV_n by auto
 qed auto
-
 
 abbreviation validStar :: \<open>(('i, 'w) kripke \<Rightarrow> bool) \<Rightarrow> 'i fm set \<Rightarrow> 'i fm \<Rightarrow> bool\<close>
   (\<open>_; _ \<TTurnstile>\<star> _\<close> [50, 50, 50] 50) where
@@ -208,6 +199,9 @@ primrec eval :: \<open>(id \<Rightarrow> bool) \<Rightarrow> ('i fm \<Rightarrow
 | \<open>eval g h (p \<^bold>\<and> q) = (eval g h p \<and> eval g h q)\<close>
 | \<open>eval g h (p \<^bold>\<longrightarrow> q) = (eval g h p \<longrightarrow> eval g h q)\<close>
 | \<open>eval _ h (K i p) = h (K i p)\<close>
+| \<open>eval _ h (Ev g p) = h (Ev g p)\<close>
+| \<open>eval _ h (Co g p) = h (Co g p)\<close>
+| \<open>eval _ h (Di g p) = h (Di g p)\<close>
 
 abbreviation \<open>tautology p \<equiv> \<forall>g h. eval g h p\<close>
 
@@ -242,7 +236,7 @@ section \<open>Soundness\<close>
 
 lemma eval_semantics:
   \<open>eval (pi w) (\<lambda>q. \<lparr>\<W> = W, \<K> = r, \<pi> = pi\<rparr>, w \<Turnstile> q) p = (\<lparr>\<W> = W, \<K> = r, \<pi> = pi\<rparr>, w \<Turnstile> p)\<close>
-  sorry (*by (induct p) simp_all*)
+  by (induct p) simp_all
 
 lemma tautology:
   assumes \<open>tautology p\<close>
@@ -924,6 +918,15 @@ next
         using Cons by auto
     qed simp
   qed
+next
+  case (Ev g p)
+  then show ?case sorry
+next
+  case (Co g p)
+  then show ?case sorry
+next
+  case (Di g p)
+  then show ?case sorry
 qed
 
 lemma canonical_model:
